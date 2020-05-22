fdf0dd041df1caf633f2c0288e9165323ca56a6b
Harry Cai <harry.cai@hotmail.com>
2020-05-22 13:47:19 +0100

    Add natspec comments to contract

--- contracts/FederatedLearning.sol
+++ contracts/FederatedLearning.sol
@@ -1,34 +1,35 @@
 pragma solidity >=0.4.21 <0.7.0;
 
-
+/// @title Records contributions made to a consortium Federated Learning process
+/// @author Harry Cai
 contract FederatedLearning {
-    // Address of contract creator, who evaluates updates
+
+    /// @notice Address of contract creator, who evaluates updates
     address public evaluator;
 
-    // Addresses of registered trainers
+    /// @notice Addresses of registered trainers
     address[] public trainers;
 
-    // IPFS hash of genesis model
+    /// IPFS hash of genesis model
     bytes32 public genesis;
 
-    // Mapping of trainer addresses to IPFS hashes of model updates in the previous training round.
-    // Clients will need to download all of these and train from their aggregate.
-    // IPFS hashes are 34 bytes long but we discard the first two (0x1220), which indicate hash function and length.
-    // https://ethereum.stackexchange.com/questions/17094/how-to-store-ipfs-hash-using-bytes
+    /// @notice Mapping of trainer addresses to IPFS hashes of model updates in the previous training round. Clients will need to download all of these and train from their aggregate.
+    /// @dev IPFS hashes are 34 bytes long but we discard the first two (0x1220), which indicate hash function and length. https://ethereum.stackexchange.com/questions/17094/how-to-store-ipfs-hash-using-bytes
     mapping(address => bytes32) public previousUpdates;
 
-    // Mapping of trainer addresses IPFS hashes of model updates in the current training round.
+    /// @notice Mapping of trainer addresses IPFS hashes of model updates in the current training round.
     mapping(address => bytes32) public currentUpdates;
 
-    // Index of training round. Starts at zero.
+    /// @notice Index of training round. Starts at zero.
     uint256 public trainingRound;
 
-    // Number of contributions each trainer has made
+    /// @notice Number of contributions each trainer has made
     mapping(address => uint256) public tokens;
 
-    // Number of tokens issued and owned by trainers.
+    /// @notice Number of tokens issued and owned by trainers.
     uint256 public totalTokens;
 
+    /// @notice Constructor. The address that deploys the contract is set as the evaluator.
     constructor() public {
         evaluator = msg.sender;
     }
@@ -51,14 +52,17 @@ contract FederatedLearning {
         _;
     }
 
+    /// @return Token count of the calling address.
     function getTokens() external view returns (uint256) {
         return tokens[msg.sender];
     }
 
+    /// @return List of addresses of registered trainers.
     function getTrainers() external view returns (address[] memory) {
         return trainers;
     }
 
+    /// @return Whether the calling address is a registered trainer.
     function isTrainer() external view returns (bool) {
         return isTrainer(msg.sender);
     }
@@ -73,6 +77,7 @@ contract FederatedLearning {
         return false;
     }
 
+    /// @return Whether the current training round is finished.
     function isTrainingRoundFinished() public view returns (bool) {
         // check if currentUpdates maps to zero for any trainer
         for (uint256 i = 0; i < trainers.length; i++) {
@@ -84,6 +89,7 @@ contract FederatedLearning {
         return true;
     }
 
+    /// @notice Starts or resets training and sets the genesis model.
     function setGenesis(bytes32 _modelHash) external evaluatorOnly() {
         for (uint256 i = 0; i < trainers.length; i++) {
             address trainer = trainers[i];
@@ -98,11 +104,13 @@ contract FederatedLearning {
         trainingRound = 0;
     }
 
+    /// @notice Registers the calling address as a trainer.
     function addTrainer() external {
         require(!isTrainer(msg.sender), "Trainer already added");
         trainers.push(msg.sender);
     }
 
+    /// @notice Records a training contribution in the current round.
     function addModelUpdate(bytes32 _modelHash) external trainersOnly() {
         require(
             currentUpdates[msg.sender] == 0,
@@ -111,6 +119,9 @@ contract FederatedLearning {
         currentUpdates[msg.sender] = _modelHash;
     }
 
+    /// @notice Rewards a trainer with tokens.
+    /// @param _trainer The address of the trainer to give tokens to
+    /// @param _numTokens The number of tokens to award; should be based on marginal value contribution
     function giveTokens(address _trainer, uint256 _numTokens)
         external
         evaluatorOnly()
@@ -125,6 +136,7 @@ contract FederatedLearning {
         totalTokens = totalTokens + _numTokens;
     }
 
+    /// @notice Ends the current training round and starts the next one.
     function nextTrainingRound()
         external
         evaluatorOnly()
