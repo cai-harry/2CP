bbd001d9f837e519f9a5950c6a69a86e71a61e5a
Harry Cai <harry.cai@hotmail.com>
2020-06-02 20:49:53 +0100

    Make covid experiment work

--- experiments/covid_hackathon.py
+++ experiments/covid_hackathon.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 from context import Client
 from context import print_global_performance, print_token_count
@@ -21,18 +22,18 @@ class CovidData:
         )
         self.data = self._convert_to_tensor(X)
         self.targets = self._convert_to_tensor(y).unsqueeze(dim=-1)
+        assert len(self.data) == len(self.targets), \
+            f"Data and targets have different lengths {len(self.data)} and {len(self.targets)}"
 
-    def split_3(self):
-        left = len(self.data) // 3
-        right = 2 * len(self.data) // 3
-        return (
-            self.data[:left],
-            self.targets[:left],
-            self.data[left:right],
-            self.targets[left:right],
-            self.data[right:],
-            self.targets[right:]
-        )
+    def split(self, n):
+        perm = torch.randperm(len(self.data))
+        chunks = torch.chunk(perm, n)
+        output = []
+        for c in chunks:
+            output.append(self.data[c])
+            output.append(self.targets[c])
+        return output
+
 
     def _split_into_lists(self, data, labels):
         record_list = list()
@@ -60,9 +61,9 @@ class CovidModel(nn.Module):
         self.dropout = nn.Dropout(p=0.25)
 
     def forward(self, x):
-        x = torch.relu(self.fc1(x))
+        x = F.relu(self.fc1(x))
         x = self.dropout(x)
-        x = torch.relu(self.fc2(x))
+        x = F.relu(self.fc2(x))
         x = self.dropout(x)
         x = torch.sigmoid(self.fc3(x))
         return x
@@ -70,10 +71,8 @@ class CovidModel(nn.Module):
 
 
 alice_data, alice_targets, bob_data, bob_targets, \
-     charlie_data, charlie_targets = CovidData().split_3()
-
-print(alice_data.shape)
-print(alice_targets.shape)
+     charlie_data, charlie_targets, david_data, david_targets, \
+         eve_data, eve_targets = CovidData().split(5)
 
 # These clients will evaluate
 alice = Client("Alice", alice_data, alice_targets, CovidModel, 0)
@@ -81,6 +80,8 @@ alice = Client("Alice", alice_data, alice_targets, CovidModel, 0)
 # These clients will train
 bob = Client("Bob", bob_data, bob_targets, CovidModel, 1)
 charlie = Client("Charlie", charlie_data, charlie_targets, CovidModel, 2)
+david = Client("David", david_data, david_targets, CovidModel, 3)
+eve = Client("Eve", eve_data, eve_targets, CovidModel, 4)
 
 TRAINING_ITERATIONS = 16
 TRAINING_HYPERPARAMETERS = {
@@ -93,7 +94,12 @@ for i in range(1, TRAINING_ITERATIONS+1):
     print(f"\nIteration {i}")
     bob.run_training_round(**TRAINING_HYPERPARAMETERS)
     charlie.run_training_round(**TRAINING_HYPERPARAMETERS)
+    david.run_training_round(**TRAINING_HYPERPARAMETERS)
+    eve.run_training_round(**TRAINING_HYPERPARAMETERS)
     print_global_performance(alice)
     alice.finish_training_round()
+    print_token_count(alice)
     print_token_count(bob)
     print_token_count(charlie)
+    print_token_count(david)
+    print_token_count(eve)
