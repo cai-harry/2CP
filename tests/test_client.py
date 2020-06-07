401699c644cd2679f3a3622969df0b732ea1e7e9
Harry Cai <harry.cai@hotmail.com>
2020-06-07 21:02:08 +0100

    Assert same global model aggregations

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,3 +1,5 @@
+import filecmp
+
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
@@ -6,6 +8,7 @@ from context import Client
 from context import print_global_performance, print_token_count
 
 from test_utils.xor import XORDataset, XORModel, plot_predictions
+
 
 def test_integration():
 
@@ -19,7 +22,8 @@ def test_integration():
     torch.manual_seed(TORCH_SEED)
 
     alice_data, alice_targets = XORDataset(256).get()
-    bob_data, bob_targets, charlie_data, charlie_targets = XORDataset(128).split()
+    bob_data, bob_targets, charlie_data, charlie_targets = XORDataset(
+        128).split()
     david_data, david_targets, eve_data, eve_targets = XORDataset(4).split()
 
     # These clients will evaluate
@@ -30,7 +34,6 @@ def test_integration():
     charlie = Client("Charlie", charlie_data, charlie_targets, XORModel, 2)
     david = Client("David", david_data, david_targets, XORModel, 3)
     eve = Client("Eve", eve_data, eve_targets, XORModel, 4)
-
 
     alice.set_genesis_model()
     for i in range(1, TRAINING_ITERATIONS+1):
@@ -46,7 +49,34 @@ def test_integration():
         print_token_count(david)
         print_token_count(eve)
 
-    assert bob.get_token_count() > david.get_token_count()
-    assert bob.get_token_count() > eve.get_token_count()
-    assert charlie.get_token_count() > david.get_token_count()
-    assert charlie.get_token_count() > eve.get_token_count()
+    assert bob.get_token_count() > david.get_token_count(
+    ), "Bob ended up with fewer tokens than David"
+    assert bob.get_token_count() > eve.get_token_count(
+    ), "Bob ended up with fewer tokens than Eve"
+    assert charlie.get_token_count() > david.get_token_count(
+    ), "Charlie ended up with fewer tokens than David"
+    assert charlie.get_token_count() > eve.get_token_count(
+    ), "Charlie ended up with fewer tokens than Eve"
+    
+    alice_global_model = alice._get_current_global_model()
+    bob_global_model = bob._get_current_global_model()
+
+    assert _same_weights(
+        alice_global_model,
+        bob_global_model
+    ), "Alice and Bob ran the same aggregation but got different model weights"
+
+    print(f"Alice: {alice_global_model.state_dict()}")
+    assert str(alice_global_model.state_dict()) == \
+        str(bob_global_model.state_dict()), \
+            "Alice and Bob ran the same aggregation but got different model dicts"
+
+def _same_weights(model_a, model_b):
+    """
+    Checks if two pytorch models have the same weights.
+    https://discuss.pytorch.org/t/check-if-models-have-same-weights/4351/5
+    """
+    for params_a, params_b in zip(model_a.parameters(), model_b.parameters()):
+        if (params_a.data!=params_b.data).sum() > 0:
+            return False
+    return True
