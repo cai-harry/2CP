1d38bfbb0f4dcb9bf76fa1c94ffa4d2b4435728d
Harry Cai <harry.cai@hotmail.com>
2020-06-20 15:55:42 +0100

    Make covid_hackathon.py work again

--- experiments/covid_hackathon.py
+++ experiments/covid_hackathon.py
@@ -6,7 +6,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from client import CrowdsourceClient
+from clients import CrowdsourceClient
 from utils import print_global_performance, print_token_count
 
 
