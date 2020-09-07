d0a34e34e5aaae987ffd3c51eee133e134cd1beb
Harry Cai <harry.cai@hotmail.com>
2020-09-07 19:06:49 +0100

    Run more MNIST DP experiments
    Varying noise multiplier as requested

--- experiments/mnist/mnist.py
+++ experiments/mnist/mnist.py
@@ -24,11 +24,23 @@ if __name__ == "__main__":
         'epochs': 1,
         'learning_rate': 1e-2
     }
-    DP_PARAMS = {
-        'l2_norm_clip': 1.0,
-        'noise_multiplier': 1.1,
-        'delta': 1e-5
-    }
+    DP_PARAMS = [
+        {
+            'l2_norm_clip': 1.0,
+            'noise_multiplier': 0.9,
+            'delta': 1e-5
+        },
+        {
+            'l2_norm_clip': 1.0,
+            'noise_multiplier': 1.3,
+            'delta': 1e-5
+        },
+        {
+            'l2_norm_clip': 1.0,
+            'noise_multiplier': 1.5,
+            'delta': 1e-5
+        },
+    ]
     ROUND_DURATION = 1800  # should always end early
  
     runner = ExperimentRunner(
@@ -49,24 +61,80 @@ if __name__ == "__main__":
     else:
         experiments = [
             # Test A
-            {'dataset': 'mnist', 'split_type': 'equal', 'num_trainers': 4},
+            # {'dataset': 'mnist', 'split_type': 'equal', 'num_trainers': 4},
 
             # Test B
-            {'dataset': 'mnist', 'split_type': 'size', 'num_trainers': 4, 'ratios': [1,2,3,4]},
+            # {'dataset': 'mnist', 'split_type': 'size', 'num_trainers': 4, 'ratios': [1,2,3,4]},
 
             # Test C
-            {'dataset': 'mnist', 'split_type': 'flip', 'num_trainers': 4, 'flip_probs': [0,0.1,0.2,0.3]},
+            # {'dataset': 'mnist', 'split_type': 'flip', 'num_trainers': 4, 'flip_probs': [0,0.1,0.2,0.3]},
 
             # Test D
-            {'dataset': 'mnist', 'split_type': 'noniid', 'num_trainers': 4, 'disjointness': 0.6},
+            # {'dataset': 'mnist', 'split_type': 'noniid', 'num_trainers': 4, 'disjointness': 0.6},
 
             # Test E
             {
                 'dataset': 'mnist',
                 'split_type': 'dp',
                 'num_trainers': 4,
-                'dp_params': DP_PARAMS,
+                'dp_params': DP_PARAMS[0],
                 'using_dp': [True, True, True, True]
+            },
+            {
+                'dataset': 'mnist',
+                'split_type': 'dp',
+                'num_trainers': 4,
+                'dp_params': DP_PARAMS[0],
+                'using_dp': [False, True, True, True]
+            },
+            {
+                'dataset': 'mnist',
+                'split_type': 'dp',
+                'num_trainers': 4,
+                'dp_params': DP_PARAMS[0],
+                'using_dp': [True, False, False, False]
+            },
+            {
+                'dataset': 'mnist',
+                'split_type': 'dp',
+                'num_trainers': 4,
+                'dp_params': DP_PARAMS[1],
+                'using_dp': [True, True, True, True]
+            },
+            {
+                'dataset': 'mnist',
+                'split_type': 'dp',
+                'num_trainers': 4,
+                'dp_params': DP_PARAMS[1],
+                'using_dp': [False, True, True, True]
+            },
+            {
+                'dataset': 'mnist',
+                'split_type': 'dp',
+                'num_trainers': 4,
+                'dp_params': DP_PARAMS[1],
+                'using_dp': [True, False, False, False]
+            },
+            {
+                'dataset': 'mnist',
+                'split_type': 'dp',
+                'num_trainers': 4,
+                'dp_params': DP_PARAMS[2],
+                'using_dp': [True, True, True, True]
+            },
+            {
+                'dataset': 'mnist',
+                'split_type': 'dp',
+                'num_trainers': 4,
+                'dp_params': DP_PARAMS[2],
+                'using_dp': [False, True, True, True]
+            },
+            {
+                'dataset': 'mnist',
+                'split_type': 'dp',
+                'num_trainers': 4,
+                'dp_params': DP_PARAMS[2],
+                'using_dp': [True, False, False, False]
             },
         ]
         method = 'step'
